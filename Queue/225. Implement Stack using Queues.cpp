c3c9b2d0d30ec904930ff4a2661e671d3d9b4f5d
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-08-21 17:43:03 +0530

    Update 225. Implement Stack using Queues.cpp

--- Queue/225. Implement Stack using Queues.cpp
+++ Queue/225. Implement Stack using Queues.cpp
@@ -1,21 +1,42 @@
-class MyStack {
+class Stack {
+	// Define the data members.
     queue<int> q;
-public:
-    MyStack() {}
-    
-    void push(int x) {
-        q.push(x);
-        int size = q.size();
-        int cnt=1;
 
-        while(cnt<size){
+   public:
+    Stack() {
+        // Implement the Constructor.
+        
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function.
+        return q.size();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function.
+        return q.empty();
+    }
+
+    void push(int element) {
+        // Implement the push() function.
+        q.push(element);
+
+        int size=q.size();
+        int cnt=1;
+        while(size>cnt){
             q.push(q.front());
             q.pop();
             cnt++;
         }
+
+
     }
-    
+
     int pop() {
+        // Implement the pop() function.
         if(q.empty()){
             return -1;
         }
@@ -23,12 +44,12 @@ public:
         q.pop();
         return ans;
     }
-    
+
     int top() {
-        return q.front();
-    }
-    
-    bool empty() {
-        return q.empty();
+        // Implement the top() function.'
+        if(!q.empty()){
+            return q.front();
+        }else
+            return -1;
     }
 };

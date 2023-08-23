b74b2dff4387d11e5eed9a90ca59fed6c009c651
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-08-23 16:28:26 +0530

    Create Reverse words in a given string(GFG).cpp

---
+++ String/Reverse words in a given string(GFG).cpp
@@ -0,0 +1,23 @@
+string reverseWords(string S) 
+    { 
+        // code here 
+        stack<string> st;
+        for(int i=0;i<S.length();i++){
+            string word="";
+            while(i < S.length() && S[i] != '.'){
+                word+=S[i];
+                i++;
+            }
+            st.push(word);
+        }
+       string reversed = "";
+    while(!st.empty()){
+        reversed += st.top();
+        st.pop();
+        if (!st.empty()) {
+            reversed += ".";
+        }
+    }
+    
+    return reversed;
+    } 

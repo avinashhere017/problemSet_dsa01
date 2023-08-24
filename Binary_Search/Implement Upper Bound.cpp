deea0c1bcc0e33cb7051383137d38facef1231f7
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-08-24 18:46:23 +0530

    Create Implement Upper Bound.cpp

---
+++ Binary_Search/Implement Upper Bound.cpp
@@ -0,0 +1,19 @@
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.	
+
+	int left = 0;
+	int right =n-1;
+	int ans=n;
+
+	while(left<=right){
+		int mid=(left+right)/2;
+
+		if(arr[mid]>x){
+			ans=mid;
+			
+			right=mid-1;
+                }
+                else{ left = mid + 1; }
+        }
+	return ans;
+}

071433e6430be870172c5c6466f6ec360e19bc7d
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-08-29 09:24:37 +0530

    Update level-order-traversal.cpp

--- tree/level-order-traversal.cpp
+++ tree/level-order-traversal.cpp
@@ -25,3 +25,29 @@ vector<int> levelOrder(Node* node)
       return ans;
       
     }
+
+
+
+
+// leetcode answer
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+       vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root == NULL) return ans;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> level;
+            for(int i = 0;i<n;i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                 level.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+               
+            }
+            ans.push_back(level);
+
+        }
+        return ans;

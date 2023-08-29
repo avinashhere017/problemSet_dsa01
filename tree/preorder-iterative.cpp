b29e015e9e7b67f7b7e8234e7e81bef0221aeeb4
Avinash kumar chaurasia <87019999+avinashhere017@users.noreply.github.com>
2023-08-29 09:40:32 +0530

    Create preorder-iterative.cpp

---
+++ tree/preorder-iterative.cpp
@@ -0,0 +1,21 @@
+vector <int> preorder(Node* root)
+{
+  // Your code here
+  vector<int>ans;
+  stack<Node*> st;
+  
+  st.push(root);
+  while(!st.empty()){
+      
+      Node* temp = st.top();
+      st.pop();
+      ans.push_back(temp->data);
+      if(temp->right){
+          st.push(temp->right);
+      }
+      if(temp->left){
+          st.push(temp->left);
+      }
+  }
+  return ans;
+}
